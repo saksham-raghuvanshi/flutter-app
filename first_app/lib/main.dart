da98857b16272e1a84d04a6dc10633ec2aff21e7
Saksham <sakshamraghuvanshi123@gmail.com>
2023-12-27 15:14:04 +0530

    Background Color

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text("Hello World1")));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Center(
+          child: Text("Hello World1"),
+        ),
+      ),
+    ),
+  );
 }
